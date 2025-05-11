11781bd74c25d0a4e64b769208d14e3f74133c19
MoisesVazquez123 <vazquezdiazmoises296@gmail.com>
2025-05-11 11:51:39 -0600

    Add files via upload

---
+++ js/Huron.js
@@ -0,0 +1,16 @@
+function startSlider(sliderId, interval) {
+    const slider = document.getElementById(sliderId);
+    const images = slider.querySelectorAll("img");
+    let current = 0;
+
+    setInterval(() => {
+      images[current].classList.remove("active");
+      current = (current + 1) % images.length;
+      images[current].classList.add("active");
+    }, interval);
+  }
+
+  // Iniciar sliders independientes
+  startSlider("slider1", 3000);
+  startSlider("slider2", 4000);
+  startSlider("slider3", 5000);
