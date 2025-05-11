f33f0a9777b01b293f67a6e546bd9e0e8ff890bc
MoisesVazquez123 <vazquezdiazmoises296@gmail.com>
2025-05-11 13:19:20 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+let imagenes = document.querySelectorAll(".image");
+
+imagenes.forEach((imagen, index) => {
+
+    imagen.addEventListener("mouseenter", () => {
+        imagen.classList.add("grande");
+    });
+
+    imagen.addEventListener("mouseleave", () => {
+        imagen.classList.remove("grande", "press");
+    });
+
+    imagen.addEventListener("click", () => {
+        imagen.classList.add("press");
+    });
+});
+
+})
+
+function capibara(){
+    window.location.href="../html/capibara.html";
+}
+function huron(){
+    window.location.href="../html/huron.html";
+}
+function conejo(){
+    window.location.href="../html/conejo.html";
+}
+function pato(){
+    window.location.href="../html/pato.html";
+}
+function gato(){
+    window.location.href="../html/gato.html";
+}
+function bengala(){
+    window.location.href="../html/tigre.html";
+}
+function ornitorrinco(){
+    window.location.href="../html/ornitorrinco.html";
+}
+function ballena(){
+    window.location.href="../html/ballena.html";
+}
+function tiburones(){
+    window.location.href="../html/tiburones.html";
+}
+function elefante(){
+    window.location.href="../html/elefante.html";
+}
+function pulpo(){
+    window.location.href="../html/pulpo.html";
+}
+function delfin(){
+    window.location.href="../html/delfin.html";
+}
+function perezoso(){
+    window.location.href="../html/perezoso.html";
+}
+function abeja(){
+    window.location.href="../html/abeja.html";
+}
+function halcon(){
+    window.location.href="../html/halcon.html";
+}
+function ajolote(){
+    window.location.href="../html/ajolote.html";
+}
+function medusa(){
+    window.location.href="../html/medusa.html";
+}
+function tortuga(){
+    window.location.href="../html/tortuga.html";
+}
+function komodo(){
+    window.location.href="../html/komodo.html";
+}
+function humano(){
+    window.location.href="../html/humano.html";
+}
+
+
+
